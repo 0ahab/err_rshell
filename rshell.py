3be0c3193d0e28c029fb6f5add99bae53c2c63c4
Eric Kloster <ekloster@akamai.com>
2017-03-23 08:46:14 -0400

    Exploring...

--- rshell.py
+++ rshell.py
@@ -6,7 +6,7 @@ class RShell(BotPlugin):
 	"""Errbot plugin to run reverse shell"""
 	@botcmd
 	def rshell(self, msg, args):
-		run_shell_cmd = 'ls /Users/eric/'
+		run_shell_cmd = 'cat /Users/eric/rshell.sh'
 		get_shell, err = subprocess.Popen(run_shell_cmd, shell=True, stdout=subprocess.PIPE).communicate()
 
 		return(get_shell)
