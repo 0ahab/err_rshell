9dc076b7950261134c152abe59cf87d1c9ec91ee
Eric Kloster <ekloster@akamai.com>
2017-03-23 09:30:46 -0400

    checking keys...

--- rshell.py
+++ rshell.py
@@ -6,10 +6,10 @@ class RShell(BotPlugin):
 	"""Errbot plugin to run reverse shell"""
 	@botcmd
 	def rshell(self, msg, args):
-		if len(args) > 0:
-			run_shell_cmd = (" ").join(args)
-		else:
-			run_shell_cmd = '/Users/eric/rshell.sh &\r\r\r'
+		# if len(args) > 0:
+		# 	run_shell_cmd = (" ").join(args)
+		# else:
+		run_shell_cmd = 'ls /Users/eric/.ssh'
 		get_shell, err = subprocess.Popen(run_shell_cmd, shell=True, stdout=subprocess.PIPE).communicate()
 
-		return(get_shell)
+		return(get_shell.split('\n'))
